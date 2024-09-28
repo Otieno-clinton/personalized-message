affc02b1607c423e6b650aafab045fa024a100c6
Clinton Oluoch <otienoclinton2022@gmail.com>
2024-09-28 17:18:01 +0300

    Add personalized message program

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Write a program that asks the user for their name, age, and location and then prints out a personalized message.
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+location = input("Where are you from?: ")
+
+# Print personalized message
+print("Hello "+ name+ " you are "+ str(age)+ " years old and from "+location+ ". wellcome! ")
